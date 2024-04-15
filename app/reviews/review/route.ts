f61e8af5ac217be8327464d35a25401b39f073c5
Sebastian Mohr <sebastianmohr96@gmail.com>
2024-04-15 17:07:02 +0100

    Remove unneeded file

--- app/reviews/review/route.ts
+++
@@ -1,8 +0,0 @@
-import {getSingleReview} from "@/app/helpers/fetchReviewHelper";
-
-export const dynamic = 'force-dynamic' // defaults to auto
-export async function GET(reviewId: string) {
-  const data = await getSingleReview(reviewId);
-
-  return Response.json({data})
-}
